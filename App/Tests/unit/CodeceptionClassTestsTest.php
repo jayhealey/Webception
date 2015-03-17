ee5096a589af642dbe2f051b9ca026bd621114fe
Joshua Gdovin <joshuag@focusschoolsoftware.com>
2015-03-17 12:12:11 -0400

    fix for test fail issue 38

--- App/Tests/unit/CodeceptionClassTestsTest.php
+++ App/Tests/unit/CodeceptionClassTestsTest.php
@@ -103,6 +103,7 @@ class CodeceptionClassTestsTest extends \Codeception\TestCase\Test
         $params = array(
             $this->config['executable'],        // Codeception Executable
             "run",                              // Command to Codeception
+            "--no-colors",                      // Forcing Codeception to not use colors, if enabled in codeception.yml
             "--config=\"{$codeception->site->getConfig()}\"", // Full path & file of Codeception
             $this->type,                              // Test Type (Acceptance, Unit, Functional)
             $this->test_filename,                          // Filename of the Codeception test
