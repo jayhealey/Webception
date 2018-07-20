6b7ca1379c1ec82f69e5d365c53e882fcdeb0c03
Sami Greenbury <vikingo@patabugen.co.uk>
2018-07-20 12:01:05 +0100

    Changed how we exclude intentionally failing tests from CLI testing
    Also allowed the always-passing test to run on the command line.

--- App/Tests/acceptance/TheTestThatFailsCept.php
+++ App/Tests/acceptance/TheTestThatFailsCept.php
@@ -1,5 +1,16 @@
 <?php
-
+// We don't want to run this on the terminal because we only need this
+// test so we can check that the web-interface handles failures correctly. The
+// terminal is for checking that Webception actually works - so having fake
+// failures isn't helpful.
+// 
+// When running the tests on the command line, you should add
+// --skip-group=web-interface-only
+// 
+// after 'run'. E.g: `codecept run --skip-group=web-interface-only acceptance`
+// 
+// @group web-interface-only
+// 
 /*
  * This file is part of the Webception package.
  *
@@ -9,13 +20,8 @@
  * file that was distributed with this source code.
  */
 
-// This test will be skipped when running Codeception from the terminal.
-
 // This test is used as a false positive when testing Webception. This test is used
 // to test a failed test via the Webception RUN command.
-
-if (isset($_SERVER['TERM_PROGRAM']) || isset($_SERVER['TERM']))
-    $scenario->skip();
 
 $I = new WebGuy($scenario);
 $I->wantTo('fail at passing');
